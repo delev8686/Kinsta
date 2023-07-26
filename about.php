a68dd66345acfc1db08d0022a5dd8172d2216c04
delev8686 <91798160+delev8686@users.noreply.github.com>
2023-07-26 22:59:40 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,45 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <style>
+        /* Custom CSS style */
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+        main {
+            padding: 2rem;
+        }
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+    </style>
+</head>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    <main>
+        <h2>My Story</h2>
+        <p></p>
+        
+        <h2>My Mission</h2>
+        <p></p>
+    </main>
+    <footer>
+        <p>Contact me at: idelev@kinsta.com</p>
+    </footer>
+</body>
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<head>
+    <title>My Kinsta Static Website</title>
+</head>
+<body>
+
+<header>
+    <h1>Welcome to My Kinsta Static  Website :) </h1>
+</header>
+    <link rel="stylesheet" href="styles.css">
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
+
+<section id="home">
+    <h2>Home</h2>
+    <p>This is the home section of the website.</p>
+</section>
+
+<section id="about">
+    <h2>About</h2>
+    <p>This is the about section of the website.</p>
+</section>
+
+<section id="contact">
+    <h2>Contact</h2>
+    <p>This is the contact section of the website.</p>
+    <p>Email: idelev@kinsta.com</p>
+</section>
+
+<footer>
+    <p>&copy; 2023 My Kinsta Static Website. All rights reserved.</p>
+</footer>
+
+</body>
+
