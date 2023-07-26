0a422c15241d7b9e736907c62310a5a0382d1307
delev8686 <91798160+delev8686@users.noreply.github.com>
2023-07-26 23:32:52 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <style>
+        /* Custom CSS style */
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+        }
+        header {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+        main {
+            padding: 2rem;
+        }
+        footer {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 1rem;
+        }
+    </style>
+</head>
+<nav>
+    <ul>
+        <li><a href="index.php">Back to Home</a></li>
+    </ul>
+</nav>
+<body>
+    <header>
+        <h1>About Us</h1>
+    </header>
+    <main>
+        <h2>My Story</h2>
+        <p></p>
+        
+        <h2>My Mission</h2>
+        <p></p>
+    </main>
+    <footer>
+        <p>Contact me at: idelev@kinsta.com</p>
+    </footer>
+</body>
+
---
+++ index.php
@@ -0,0 +1,75 @@
+<head>
+    <title>My Kinsta Static Website</title>
+</head>
+<body>
+
+<header>
+    <h1>Welcome to My Kinsta Static  Website :) </h1>
+</header>
+    <link rel="stylesheet" href="styles.css">
+<nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="about.php">About</a></li>
+        <li><a href="contact.php">Contact</a></li>
+    </ul>
+</nav>
+
+<section id="home">
+    <h2>Home</h2>
+    <p>This is the home section of the website.</p>
+</section>
+
+<section id="about">
+    <h2>About</h2>
+    <p>This is the about section of the website.</p>
+</section>
+
+<section id="contact">
+    <h2>Contact</h2>
+    <p>This is the contact section of the website.</p>
+    <p>Email: idelev@kinsta.com</p>
+</section>
+
+<footer>
+    <p>&copy; 2023 My Kinsta Static Website. All rights reserved.</p>
+</footer>
+<!DOCTYPE html>
+<html>
+	
+<head>
+	<title>
+		How to call PHP function
+		on the click of a Button ?
+	</title>
+</head>
+
+<body style="text-align:center;">
+	
+	<h1 style="color:green;">
+	</h1>
+	
+
+	<?php
+	
+		if(isset($_POST['button1'])) {
+			echo "I love Kinsta!";
+		}
+		if(isset($_POST['button2'])) {
+			echo "Kinsta is the best company hosting provider!";
+		}
+	?>
+	
+	<form method="post">
+		<input type="submit" name="button1"
+				value="Kinsta_Love"/>
+		
+		<input type="submit" name="button2"
+				value="Which_is_the_best_company?"/>
+	</form>
+</body>
+
+</html>
+
+</body>
+
